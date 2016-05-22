00021ac8ded86d4d017038425c6517be06d60a95
Dustin <hiitsdustindavis@gmail.com>
2016-05-22 16:17:56 -0700

    add header toggle class jquery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+var prev = 0;
+$(window).scroll(function() {
+  var header = $('.header');
+  var scrollTop = $(this).scrollTop();
+  header.toggleClass('hidden', scrollTop > prev);
+  console.log(scrollTop);
+  prev = scrollTop;
+});
