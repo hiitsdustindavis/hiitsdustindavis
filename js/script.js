291020049818bacc38d9a9c8ee2aeecdf20a07c8
Dustin <hiitsdustindavis@gmail.com>
2016-06-29 15:01:37 -0700

    add projects nav markup

--- js/script.js
+++ js/script.js
@@ -3,6 +3,5 @@ $(window).scroll(function() {
   var header = $('.header');
   var scrollTop = $(this).scrollTop();
   header.toggleClass('hidden', scrollTop > prev);
-  console.log(scrollTop);
   prev = scrollTop;
 });
