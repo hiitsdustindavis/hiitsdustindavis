c9d2e7e99a9a3d2fd75ca35627b72fadee181b52
Dustin <hiitsdustindavis@gmail.com>
2016-06-29 21:43:58 -0700

    adjust scroll animation to retain project nav yellow highlighted background

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top - 80
+	        'scrollTop': $target.offset().top - 79
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
